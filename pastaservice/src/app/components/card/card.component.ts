1592879d84e056dcb1797979286829f7f54bc033
Ênnya Gomes <ennyagomesoc@gmail.com>
2024-02-26 11:02:25 -0300

    .

--- pastaservice/src/app/components/card/card.component.ts
+++ pastaservice/src/app/components/card/card.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { PokemonData } from 'src/app/models/pokemonData';
+import { PokemonService } from 'src/app/services/pokemon.service';
 
 @Component({
   selector: 'app-card',
@@ -6,11 +8,48 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./card.component.css']
 })
 export class CardComponent implements OnInit {
-  name:string = "BULBASSAUER"
-  attributesTypes:string[]= ['FIRE', 'ROCK']
-  constructor() { }
+  pokemon:PokemonData 
+  
+  constructor(
+    private service:PokemonService
+  ) { 
 
-  ngOnInit(): void {
+    this.pokemon = { 
+      id:0,
+      name:'',
+      sprites:{
+        front_default:''
+      },
+      types:[]
+    }
   }
 
+  ngOnInit(): void {
+    this.getPokemon('pikachu')
+  
+  } 
+
+  getPokemon(searchName:string){
+    this.service.getPokemon(searchName).subscribe(
+      {
+        next: (res) => {
+          this.pokemon={
+            id: res.id,
+            name: res.name,
+            sprites: res.sprites,
+            types: res.types
+          }
+          console.log(res)
+          console.log(res.types)
+          console.log(this.pokemon);
+          
+        },
+        error: (err) => console.log('not found');
+        
+        
+      }
+      )
+    
+    
+  }
 }
